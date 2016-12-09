0678af4a39ae7baa10074755a26af93331ddf8e6
suryadavinci <suryadavinci@gmail.com>
2016-12-09 16:23:45 +0530

    added

---
+++ LearnJava/src/com/suryatechsources/Call.java
@@ -0,0 +1,22 @@
+package com.suryatechsources;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Scanner;
+
+public class Call {
+	
+	public static String test="";
+	
+
+	public static void main(String[] args) throws IOException {
+		
+		
+	}
+	
+
+}
+
--- LearnJava/src/com/suryatechsources/PracticeEverything.java
+++ LearnJava/src/com/suryatechsources/PracticeEverything.java
@@ -1,137 +1,28 @@
 package com.suryatechsources;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.ListIterator;
-import java.util.Collection;
-import java.util.Iterator;
 
 
-public class PracticeEverything implements List{
+
+public class PracticeEverything {
 	
 	
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		
-		
-		
-		
+	 public String value;
+	Integer x=new Integer(5);
+	
+	
+	
+	
+	public PracticeEverything()
+	{
+		this.value="5";
 	}
-
-	public boolean add(Object arg0) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	public void add(int arg0, Object arg1) {
-		// TODO Auto-generated method stub
+	
+	public PracticeEverything(String value)
+	{
+		this.value=value;
 		
 	}
-
-	public boolean addAll(Collection arg0) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	public boolean addAll(int arg0, Collection arg1) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	public void clear() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	public boolean contains(Object arg0) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	public boolean containsAll(Collection arg0) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	public Object get(int arg0) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public int indexOf(Object arg0) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	public boolean isEmpty() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	public Iterator iterator() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public int lastIndexOf(Object arg0) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	public ListIterator listIterator() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public ListIterator listIterator(int arg0) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public boolean remove(Object arg0) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	public Object remove(int arg0) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public boolean removeAll(Collection arg0) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	public boolean retainAll(Collection arg0) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	public Object set(int arg0, Object arg1) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public int size() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	public List subList(int arg0, int arg1) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public Object[] toArray() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public Object[] toArray(Object[] arg0) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	
+	
+	
 
 }
