1aa75d062649f4a3081d32f275878ab5919c76f7
gnanasuriyan.g <gnanasuriyan.g@D7240.prodapt.com>
2017-01-13 12:24:59 +0530

    comit

--- LearnJava/src/com/suryatechsources/Call.java
+++ LearnJava/src/com/suryatechsources/Call.java
@@ -5,18 +5,52 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.LinkedList;
 import java.util.Scanner;
 
 public class Call {
 	
-	public static String test="";
 	
+	
+	Call()
+	{
+		System.out.println("no-arg cons");
+	}
+	
+	Call(int x)
+	{
+		System.out.println("arg cons");
+	}
+	
+	{
+		System.out.println("inst 1");
+	}
+	static
+	{
+		
+		System.out.println("static 1");
+	}
 
+	{
+		System.out.println("inst 2");
+	}
+	static
+	{
+		
+		System.out.println("static 2");
+	}
+	
 	public static void main(String[] args) throws IOException {
+		
+		new Call();
+		new Call(7);
+		
+		
+		
+		
 		
 		
 	}
 	
-
 }
 
--- LearnJava/src/com/suryatechsources/PracticeEverything.java
+++ LearnJava/src/com/suryatechsources/PracticeEverything.java
@@ -1,28 +1,107 @@
 package com.suryatechsources;
 
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
 
 
-public class PracticeEverything {
+class Parent {
 	
-	
-	 public String value;
-	Integer x=new Integer(5);
-	
-	
-	
-	
-	public PracticeEverything()
-	{
-		this.value="5";
+	static{
+		System.out.println("parent static");
 	}
 	
-	public PracticeEverything(String value)
 	{
-		this.value=value;
+		System.out.println("parent instance");
+	}
+	
+	Parent(){
+		System.out.println("parent cons");
+	}
+}
+
+
+class Child extends Parent
+{
+	static{
+		System.out.println("child static");
+	}
+	{
+		System.out.println("child instance");
+	}
+
+	Child(){
+		System.out.println("child cons");
+	}
+	
+	
+	
+	
+	
+}
+
+
+public class PracticeEverything extends Child{
+	
+	
+	public static void main(String [] args) {
+
+
 		
-	}
+		LinkedList ll= new LinkedList();
+		
+		ll.add(5);
+		ll.add(6);
+		ll.add(5);
+		ll.add(6);
+		
+		ll.add(5);
+		ll.add(6);
+		
+		
+		System.out.println(ll);
+		
+		
+		
+		
+		
+		
+		
+		 }
 	
+/*
+	public static void main(String[] args) {
+
+		Runtime rt=Runtime.getRuntime();
+		
+		System.out.println("Total Memory :"+rt.totalMemory());
+		
+		
+		System.out.println("Free Memory before Date Object Creation :"+rt.freeMemory());
+		
+		Date d=null;
+		
+		for(int i=0;i<1000;i++)
+		{
+			d= new Date();
+			d=null;
+		}
+		
+		
+		System.out.println("Free Memory after Date Object Creation :"+rt.freeMemory());
+		
+		
+		System.out.println("collecting garbage using Runtime");
+		Runtime.getRuntime().gc();
+
+
+		System.out.println("Free Memory after Garbage Collection :"+rt.freeMemory());
+		
+		
+		
+	}*/
 	
 	
 
 }
+
