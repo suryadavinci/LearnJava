570d89f4fcba44f2d2b0fada8afb2c22bdeaf3ac
surya <surya@SuryaLinuxMintLenovo>
2017-02-01 00:35:14 +0530

    basic

--- LearnJava/src/com/suryatechsources/DoAnything.java
+++ LearnJava/src/com/suryatechsources/DoAnything.java
@@ -2,9 +2,17 @@ package com.suryatechsources;
 
 public class DoAnything {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
 
+		
+		
+		Math.random();
+		
+
+		
+		
+		
 	}
 
 }
