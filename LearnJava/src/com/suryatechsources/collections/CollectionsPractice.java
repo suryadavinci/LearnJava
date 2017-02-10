087c300c059d6c373146e10d4dc11a386213caf9
gnanasuriyan.g <gnanasuriyan.g@D7240.prodapt.com>
2017-02-10 15:32:36 +0530

    Collection Practice

---
+++ LearnJava/src/com/suryatechsources/collections/CollectionsPractice.java
@@ -0,0 +1,17 @@
+package com.suryatechsources.collections;
+
+import java.util.Collections;
+
+public class CollectionsPractice {
+	public static void main(String[] args) {
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
