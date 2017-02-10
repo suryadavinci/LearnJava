4721ff0ad6db3c768fc9fa3dca6a687bb1325bd6
gnanasuriyan.g <gnanasuriyan.g@D7240.prodapt.com>
2017-02-10 17:13:28 +0530

    commit

--- LearnJava/src/com/suryatechsources/collections/CollectionsPractice.java
+++ LearnJava/src/com/suryatechsources/collections/CollectionsPractice.java
@@ -1,12 +1,22 @@
 package com.suryatechsources.collections;
 
 import java.util.Collections;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
 
 public class CollectionsPractice {
 	public static void main(String[] args) {
 		
 		
 		
+		
+		
+		Set<String> s= new HashSet<String>();
+		
+		
+		s.add("hello");
+		s.add("hello");
 		
 		
 		
