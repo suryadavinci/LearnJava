fc7d38e858fb7db69e8936c4fdf0f618fc5049c4
surya <surya@SuryaLinuxMintLenovo.com>
2017-02-14 23:23:12 +0530

    Inner Class

---
+++ LearnJava/src/com/suryatechsources/innerclasses/ParentClass.java
@@ -0,0 +1,26 @@
+package com.suryatechsources.innerclasses;
+
+class NotParentClass{
+	
+	class InnerClass{
+		
+	}
+}
+
+class InnerClass{
+	static int a=0;
+}
+public class ParentClass {
+	
+	static class InnerClass{
+		public static int a;
+	}
+	
+	public static void main(String[] args) {
+		InnerClass.a =5;
+		
+		
+		System.out.println();
+	}
+
+}
