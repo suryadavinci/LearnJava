459fb65ca8647504915dca30c6adf79ba6cbce4b
surya <surya@SuryaLinuxMintLenovo.com>
2017-02-25 22:15:23 +0530

    java

--- LearnJava/src/com/suryatechsources/DoAnything.java
+++ LearnJava/src/com/suryatechsources/DoAnything.java
@@ -1,11 +1,29 @@
 package com.suryatechsources;
 
 public class DoAnything {
-
+	
+	boolean quit;
+	static int count=0;
 	public static void main(String[] args) {
 
-		Math.random();
 
+		DoAnything qr = new DoAnything();
+	    
+
+
+	    System.out.println(
+	        "JMS Ready To Receive Messages (To quit, send a \"quit\" message).");
+
+	    // Wait until a "quit" message has been received.
+	    synchronized(qr) {
+	     while (! qr.quit) {
+	       try {
+	         qr.wait();
+	       } catch (InterruptedException ie) {}
+	     }
+	    }
+		
+		
 	}
 
 }
