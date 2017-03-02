67086c61a3e85cd453cb5ef4a315d0508eccdc23
surya <surya@SuryaLinuxMintLenovo.com>
2017-03-02 23:39:35 +0530

    New Line

---
+++ LearnJava/src/com/suryatechsources/TimeTakenToExecuteInstructions.java
@@ -0,0 +1,24 @@
+package com.suryatechsources;
+
+public class TimeTakenToExecuteInstructions {
+
+	public static void main(String[] args) {
+		
+		long now;
+		long then;
+		System.out.println(then=System.nanoTime());
+		System.out.println(((now=System.nanoTime())-then));
+		System.out.println(((then=System.nanoTime())-now));
+		System.out.println(((now=System.nanoTime())-then));
+		System.out.println(((then=System.nanoTime())-now));
+		System.out.println(((now=System.nanoTime())-then));
+		System.out.println(((then=System.nanoTime())-now));
+		System.out.println(((now=System.nanoTime())-then));
+		System.out.println(((then=System.nanoTime())-now));
+		System.out.println(((now=System.nanoTime())-then));
+		System.out.println(((then=System.nanoTime())-now));
+		System.out.println(((now=System.nanoTime())-then));
+		System.out.println(((then=System.nanoTime())-now));
+
+	}
+}
