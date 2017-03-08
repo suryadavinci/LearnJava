81d1a8268ef796bb3763addcaca5d4094dfa08cf
gnanasuriyan.g <gnanasuriyan.g@D7240.prodapt.com>
2017-03-08 11:32:21 +0530

    its interesting

---
+++ LearnJava/src/com/suryatechsources/NullReferenceAccess.java
@@ -0,0 +1,18 @@
+package com.suryatechsources;
+
+public class NullReferenceAccess {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		NullReferenceAccess nra = null;
+		
+		nra.nullReference();
+		
+	}
+
+	public static void nullReference(){
+		System.out.println("Accessing");
+	}
+}
