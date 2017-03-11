6cce21a8ee20a51cadf2c91849726ec85bd5abbc
surya <surya@SuryaLinuxMintLenovo.com>
2017-03-12 01:39:21 +0530

    formatted

--- LearnJava/src/com/suryatechsources/NullReferenceAccess.java
+++ LearnJava/src/com/suryatechsources/NullReferenceAccess.java
@@ -5,18 +5,16 @@ public class NullReferenceAccess {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		String x="hello";
-		int i=0;
-		while(i++<100){
-			x=x+x;
-			
+		String x = "hello";
+		int i = 0;
+		while (i++ < 100) {
+			x = x + x;
+
 		}
-		
-		
-		
+
 	}
 
-	public static void nullReference(){
+	public static void nullReference() {
 		System.out.println("Accessing");
 	}
 }
