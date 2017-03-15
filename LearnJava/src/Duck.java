34b12025c9ac963bf22408c5bcf517ccdf7f149e
surya <surya@SuryaLinuxMintLenovo.com>
2017-03-15 23:52:16 +0530

    Design Pattern Link added

---
+++ LearnJava/src/Duck.java
@@ -0,0 +1,26 @@
+
+public class Duck {
+	
+	public void quack(){
+		System.out.println("Quack Quack");
+	}
+	
+	public void swim(){
+		System.out.println("Swim Swim");
+	}
+	public void fly(){
+		System.out.println("I can Fly");
+	}
+
+}
+
+class rubberDuck extends Duck implements Flys{
+	/*public void fly(){
+		System.out.println("Nope. Can't fly.");
+	}*/
+}
+
+
+interface Flys{
+	public void fly();
+}
